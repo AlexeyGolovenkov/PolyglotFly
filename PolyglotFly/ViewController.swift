e55e2bb91f3a7b872e14c287888b035d0db52d31
Alexey Golovenkov <mobidevelop@gmail.com>
2023-07-27 18:33:22 +0400

    Switch locale on the fly

--- PolyglotFly/ViewController.swift
+++ PolyglotFly/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum Language: String {
+        case ru
+        case en
+        case de
+    }
+    
+    @IBOutlet weak var russianButton: UIButton!
+    @IBOutlet weak var englishButton: UIButton!
+    @IBOutlet weak var dutchButton: UIButton!
+    
+    var lang: Language = .en {
+        didSet {
+            fillButtons()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fillButtons()
     }
 
-
+    func fillButtons() {
+        let bundlePath = Bundle.main.path(forResource: lang.rawValue, ofType: "lproj")
+        let bundle = Bundle(path: bundlePath!)
+        russianButton.setTitle(bundle!.localizedString(forKey: "RussianButton", value: "Russian button", table: nil), for: .normal)
+        englishButton.setTitle(bundle!.localizedString(forKey: "EnglishButton", value: "English button title", table: nil), for: .normal)
+        dutchButton.setTitle(bundle!.localizedString(forKey: "DutchButton", value: "Dutch button title", table: nil), for: .normal)
+    }
+    
+    @IBAction func russianButtonTap(_ sender: Any) {
+        lang = .ru
+    }
+    
+    @IBAction func englishButtonPressed(_ sender: Any) {
+        lang = .en
+    }
+    
+    @IBAction func dutchButtonPressed(_ sender: Any) {
+        lang = .de
+    }
 }
 
